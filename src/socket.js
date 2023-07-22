5be0fd44e8ce4d476c7e5ee9ff0c91d62b0cf195
Beatriz Silva <beagaara@gmail.com>
2023-07-22 12:54:23 +0100

    NEW URL

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,4 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 //const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://chat-app-sockets-server.netlify.app';
 
-export const socket = io('https://chat-app-sockets-server.netlify.app', { transports : ['polling'], withCredentials: true});
+export const socket = io('https://chat-application-server-mc41.onrender.com', { transports : ['polling'], withCredentials: true});
