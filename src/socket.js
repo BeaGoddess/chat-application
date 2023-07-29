b6b50aed2c940cce0e5f948990ceac1e82e2d0bb
Beatriz Silva <beagaara@gmail.com>
2023-07-29 19:22:54 +0100

    New link v2

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 //export const socket = io('https://chat-application-server-mc41.onrender.com', { transports : ['polling'], withCredentials: true});
-export const socket = io("https://chat-application-server.beagoddess.repl.com", { transports: ['polling'], withCredentials: true });
+export const socket = io("https://chat-application-server.beagoddess.repl.co", { transports: ['polling'], withCredentials: true });
 
 // https://chat-application-server.beagoddess.repl.co
