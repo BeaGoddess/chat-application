e049c559bcb146b2ff7272547701eb35c71b211e
Beatriz Silva <beagaara@gmail.com>
2024-02-24 15:18:19 +0000

    Change URL

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,12 @@
 import { io } from 'socket.io-client';
 
-//export const socket = io('https://chat-application-server-mc41.onrender.com', { transports : ['polling'], withCredentials: true});
-export const socket = io("https://chat-application-server.beagoddess.repl.co", { transports: ['polling'], withCredentials: true });
+// "undefined" means the URL will be computed from the `window.location` object
+export const socket = io("https://60b6cbc3-4f12-453a-a627-195f5dc43bc2-00-3vtzsllz4onkq.riker.replit.dev/", { transports: ['polling'], withCredentials: true });
 
-// https://chat-application-server.beagoddess.repl.co
+//export const socket = io("https://chat-application-server.beagoddess.repl.co", { transports: ['polling'], withCredentials: true });
+
+/*
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://chat-app-sockets-server.netlify.app';
+
+export const socket = io('https://chat-app-sockets-server.netlify.app');
+*/
